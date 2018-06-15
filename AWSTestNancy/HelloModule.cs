50e5005b2f4180f8abe63846c8edcd56215a4d64
ctoph13 <chriscardwell13@gmail.com>
2018-06-14 21:24:58 -0500

    Add files via upload

---
+++ AWSTestNancy/HelloModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace AWSTestNancy
+{
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = parameters => "Hello World";
+        }
+    }
+}
